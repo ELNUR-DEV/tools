b13d8d48bab6a8411baf6cd6355b5dfbecb3b428
Elnur_dev <154092958+ELNUR-DEV@users.noreply.github.com>
2024-05-31 19:49:51 +0300

    enter

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php 
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+body {
+    background-color: #d9d9d4; 
+}   
+
+header {
+    text-align: justify;
+    padding: 2em 5%;
+    background: #2c3e50;
+    color: #fff;
+}
+ 
+header::after {
+    content: '';
+    display: inline-block;
+    width: 100%;
+}
+ 
+header h1,
+header nav {
+    display: inline-block;
+    vertical-align: middle;
+}
+ 
+header h1 {
+    width: 50%;
+    text-align: left;
+    padding-top: 0.5em;
+}
+ 
+header nav {
+    padding-top: 1em;
+}
+</style>
+<header>
+    <h1>Эти парни знают свое дело!!!</h1>
+    <nav><a>Миша</a><a> PHP програмист, </a><a>а Леха познал DZEN в програмировании </a></nav>
+    
+</header>
+</body>
+</html>
+
